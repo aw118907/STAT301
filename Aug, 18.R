fc3cdc668e7e3a95bf4790db91609fbed3296b5f
aw118907 <89170400+aw118907@users.noreply.github.com>
2021-08-29 21:00:46 -0400

    Add files via upload

---
+++ Aug, 18.R
@@ -0,0 +1,21 @@
+Author: Arie Williams-Fogle Date: 8/18/2021 purpose: calculate mean, median, and mode
+
+Dummy data
+
+variable1 <- c(1, 2, 3, 4, 5)
+
+# calculate the mean/average of variable1
+
+mean <- mean(variable1)
+# 3
+
+# calculate median value
+
+median <- median(variable1)
+# 3
+
+# calculate mode value
+variable2 <- c(1, 2, 3, 4, 5, 2) # unimodal data, just one mode which is "2"
+
+# Install the external packages in R
+install.packages("modeest")
